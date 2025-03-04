3f91ca5449eb8f6c0025ce216e8ae3c4419fc3bc
Gilmar Sanchez <gilmar.sanchez@nexergroup.com>
2025-03-03 20:58:34 -0500

    Third part cinemapedia - add functions to search movies

---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,61 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+
+import '../../domain/entities/movie.dart';
+
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({required this.searchMovies});
+
+  @override
+  String get searchFieldLabel => 'Search movie...';
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      if (query.isNotEmpty)
+        FadeIn(
+          child: IconButton(
+            onPressed: () => query = '',
+            icon: Icon(Icons.clear),
+          ),
+        ),
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: Icon(
+        Icons.arrow_back_ios,
+      ),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Text('BuildResults');
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return FutureBuilder(
+      future: searchMovies(query),
+      builder: (context, snapshot) {
+      final movies = snapshot.data ?? [];
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+          final movie = movies[index];
+          return ListTile(
+            title: Text(movie.title),
+          );
+        },);
+      },
+    );
+  }
+}
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_cinemapedia/presentation/delegates/search_movie_delegate.dart';
+import 'package:flutter_application_cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
     return SafeArea(
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10,),
+        padding: EdgeInsets.symmetric(
+          horizontal: 10,
+        ),
         child: SizedBox(
           width: double.infinity,
           child: Row(
@@ -25,7 +30,15 @@ class CustomAppbar extends StatelessWidget {
               ),
               Spacer(),
               IconButton(
-                onPressed: () {},
+                onPressed: () {
+                  final movieRepository = ref.read(movieRepositoryProvider);
+                  showSearch(
+                    context: context,
+                    delegate: SearchMovieDelegate(
+                      searchMovies: movieRepository.searchMovies,
+                    ),
+                  );
+                },
                 icon: Icon(Icons.search),
               ),
             ],
